26c1d172226dea4fe778adc82956fb04461348b7
MuzafferKaanCelik <muce0001@stud.kea.dk>
2024-11-25 12:06:52 +0100

    Added Classes for packages

--- src/DomainModel/Members.java
+++ src/DomainModel/Members.java
@@ -1,13 +1,4 @@
 package DomainModel;
 
-public abstract class Members {
-    private String name;
-    private int age;
-    private String email;
-    private int userid;
-    private String gender;
-
-    public Members(String name, int age, String email, int userid, String gender){
-    }
-
+public class Members extends Person{
 }
---
+++ src/DomainModel/Person.java
@@ -0,0 +1,13 @@
+package DomainModel;
+
+public abstract class Person {
+    private String name;
+    private int age;
+    private String email;
+    private int userid;
+    private String gender;
+
+    public Person(String name, int age, String email, int userid, String gender){
+    }
+
+}
