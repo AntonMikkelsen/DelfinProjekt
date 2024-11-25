9a2bbea68d70ea9210904e5f82d4d76099b2ff51
MuzafferKaanCelik <muce0001@stud.kea.dk>
2024-11-25 12:08:52 +0100

    Added super constructors for the Person classes

--- src/DomainModel/CompSwimmer.java
+++ src/DomainModel/CompSwimmer.java
@@ -1,4 +1,7 @@
 package DomainModel;
 
-public class CompSwimmer extends {
+public class CompSwimmer extends Person{
+    public CompSwimmer(String name, int age, String email, int userid, String gender) {
+        super(name, age, email, userid, gender);
+    }
 }
--- src/DomainModel/Members.java
+++ src/DomainModel/Members.java
@@ -1,4 +1,7 @@
 package DomainModel;
 
 public class Members extends Person{
+    public Members(String name, int age, String email, int userid, String gender) {
+        super(name, age, email, userid, gender);
+    }
 }
