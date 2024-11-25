eb373675ec902490ee004f5e870f15f2f28a24d4
AntonMikkelsen <anto0004@stud.kea.dk>
2024-11-25 12:45:07 +0100

    Added addTeam and isJunior methods in Teams.java

--- src/DomainModel/Teams.java
+++ src/DomainModel/Teams.java
@@ -1,4 +1,27 @@
 package DomainModel;
 
-public abstract class Teams {
+import java.util.ArrayList;
+
+public class Teams {
+    private ArrayList<Person> Juniorhold = new ArrayList<>();
+    private ArrayList<Person> Seniorhold = new ArrayList<>();
+
+    public void addToTeam(Person person){
+        if (isJunior(person)) {
+            Juniorhold.add(person);
+            System.out.println(person + "has been added to the Junior Team");
+        }
+        else {
+            Seniorhold.add(person);
+            System.out.println(person + "has been added to the Senior Team");
+        }
+    }
+
+    public Boolean isJunior(Person person){
+        return person.getAge() < 18;
+    }
+
+
+
+
 }
