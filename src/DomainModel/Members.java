2af3689f41bd7c53b919776fc3fe226a8d542815
MuzafferKaanCelik <muce0001@stud.kea.dk>
2024-11-25 11:39:37 +0100

    Added Member class

---
+++ src/DomainModel/Members.java
@@ -0,0 +1,13 @@
+package DomainModel;
+
+public abstract class Members {
+    private String name;
+    private int age;
+    private String email;
+    private int userid;
+    private String gender;
+
+    public Members(String name, int age, String email, int userid, String gender){
+    }
+
+}
