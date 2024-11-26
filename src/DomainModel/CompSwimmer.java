c33e55744c9888824cb95dc38be78bff8ac64858
MuzafferKaanCelik <muce0001@stud.kea.dk>
2024-11-26 12:56:42 +0100

    Added a whole lot of stuff plus reworked person class

--- src/DomainModel/CompSwimmer.java
+++
@@ -1,7 +0,0 @@
-package DomainModel;
-
-public class CompSwimmer extends Person{
-    public CompSwimmer(String name, int age, String email, int userid, String gender) {
-        super(name, age, email, userid, gender);
-    }
-}
---
+++ src/ENUMS/SwimmerType.java
@@ -0,0 +1,5 @@
+package ENUMS;
+
+public enum SwimmerType {
+    RECREATIONAL, COMPETITIVE
+}
