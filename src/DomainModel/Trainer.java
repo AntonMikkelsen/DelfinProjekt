48c32816651496a7cd9c640dafc97c686b56f771
MuzafferKaanCelik <muce0001@stud.kea.dk>
2024-11-28 13:20:54 +0100

    Deleted Trainer class

--- src/DomainModel/Trainer.java
+++
@@ -1,7 +0,0 @@
-package DomainModel;
-
-public class Trainer{
-    public Trainer(String name, int age, String email, int userid, String gender) {
-       // super(name, age, email, userid, gender);
-    } // Husk at adde extends på person klasse og super parameter
-}
